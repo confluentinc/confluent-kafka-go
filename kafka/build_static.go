9c6e9c20b9668d32888389da8a1ae432e49bba83
Тимофеев Роман <cryptoloop@gmail.com>
2018-11-26 18:05:46 +0300

    fix static build

--- kafka/build_static.go
+++ kafka/build_static.go
@@ -3,5 +3,6 @@
 
 package kafka
 
-// #cgo pkg-config: rdkafka-static
+// #cgo pkg-config: rdkafka
+// #cgo LDFLAGS: -Wl,-Bstatic -lrdkafka -llz4 -lssl -lcrypto -lz -lgcc -Wl,-Bdynamic -lsasl2 -lrt -ldl -lm -lpthread
 import "C"
