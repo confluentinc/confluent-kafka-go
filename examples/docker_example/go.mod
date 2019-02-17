619ef5c20f0a7a25d2153d730d4885954bfcfe76
Tarek Badr <tarekbadrshalaan@gmail.com>
2019-02-17 20:24:56 +0200

    add docker example

---
+++ examples/docker_example/go.mod
@@ -0,0 +1,3 @@
+module github.com/tarekbadrshalaan/GoKafka
+
+require github.com/confluentinc/confluent-kafka-go v0.11.6
---
+++ examples/docker_example/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	c.SubscribeTopics([]string{"myTopic", "^aRegex.*[Tt]opic"}, nil)
+
+	for {
+		msg, err := c.ReadMessage(-1)
+		if err == nil {
+			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+		} else {
+			// The client will automatically try to recover from all errors.
+			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+		}
+	}
+
+	c.Close()
+}
