59263b30bf8fc873e36270f579ea66a1c757e927
KJ Tsanaktsidis <ktsanaktsidis@zendesk.com>
2021-06-30 12:51:20 +1000

    Switch to new semantics/signature for TLS cert callback
    As per https://github.com/edenhill/librdkafka/pull/3180 the latest
    iteration of that PR changes the way the TLS cert fetch callback works.
    This commit adjusts confluent-kafka-go to work with it.

--- kafka/tlscb_thunk.h
+++ kafka/tlscb_thunk.h
@@ -1,14 +1,12 @@
 #include "rdkafka_select.h"
 
-int cgoSSLCertVerifyCB(rd_kafka_t *rk, const char *broker_name, int32_t broker_id,
-						   int *x509_error, int depth, const char *buf, size_t size,
-                           char *errstr, size_t errstr_size, void *opaque);
+int goSSLCertVerifyCB(rd_kafka_t *rk, char *broker_name, int32_t broker_id,
+                          int *x509_error, int depth, char *buf, size_t size,
+                          char *errstr, size_t errstr_size, void *opaque);
 
 
-int cgoSSLCertFetchCB(rd_kafka_t *rk, const char *broker_name, int32_t broker_id,
-                          char *buf, size_t *buf_size, char **leaf_cert, size_t *leaf_cert_size,
-                          char **pkey, size_t *pkey_size, char *chain_certs[16],
-				          size_t chain_cert_sizes[16], rd_kafka_cert_enc_t *format,
-						  void *opaque);
+rd_kafka_cert_fetch_cb_res_t goSSLCertFetchCB(rd_kafka_t *rk, char *broker_name, int32_t broker_id,
+                                               rd_kafka_ssl_cert_fetch_cb_certs_t *certsp, char *errstr,
+                                               size_t errstr_size, void *opaque);
 
 int cgo_rd_kafka_conf_set_tls_callbacks(rd_kafka_conf_t *conf);
