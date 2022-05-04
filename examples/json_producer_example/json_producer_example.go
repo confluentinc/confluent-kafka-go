03758fb784fb48a84b184b44c41a664a5c7389df
Robert Yokota <rayokota@gmail.com>
2022-05-04 12:25:13 -0700

    Fix copyright dates

--- examples/json_producer_example/json_producer_example.go
+++ examples/json_producer_example/json_producer_example.go
@@ -2,7 +2,7 @@
 package main
 
 /**
- * Copyright 2016 Confluent Inc.
+ * Copyright 2022 Confluent Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
