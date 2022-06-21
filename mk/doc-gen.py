9c586e35ae66a5f736e65deade23103bda52791f
Emanuele Sabellico <esabellico@confluent.io>
2022-06-21 13:14:34 +0200

    fixes #723 (#787)
    changed references to the new go.dev site or to github.com

--- mk/doc-gen.py
+++ mk/doc-gen.py
@@ -5,15 +5,31 @@
 # for inclusion in Confluent doc tree.
 
 
-import subprocess, re
+import subprocess
+import re
 from bs4 import BeautifulSoup
+
+
+def convert_path(url, base_url, after):
+    relative_path = url[url.rfind(after) + len(after):]
+    if relative_path == "style.css":
+        relative_path = "styles.css"
+    return f'{base_url}/{relative_path}'
 
 
 if __name__ == '__main__':
 
+    tag = "v1.9.0"
+    base_css = "https://go.dev/css"
+    base_js = "https://go.dev/js"
+    base_src = "https://github.com/confluentinc/" + \
+               f"confluent-kafka-go/blob/{tag}"
+    base_pkg = "https://pkg.go.dev"
+
     # Use godoc client to extract our package docs
     html_in = subprocess.check_output(
-        'godoc -url=/pkg/github.com/confluentinc/confluent-kafka-go/kafka | egrep -v "^using (GOPATH|module) mode"', shell=True)
+        'godoc -url=/pkg/github.com/confluentinc/confluent-kafka-go/kafka ' +
+        '| egrep -v "^using (GOPATH|module) mode"', shell=True)
 
     # Parse HTML
     soup = BeautifulSoup(html_in, 'html.parser')
@@ -23,17 +39,24 @@ if __name__ == '__main__':
 
     # Remove "Subdirectories"
     soup.find(id='pkg-subdirectories').decompose()
-    soup.find(attrs={'class':'pkg-dir'}).decompose()
+    soup.find(attrs={'class': 'pkg-dir'}).decompose()
     for t in soup.find_all(href='#pkg-subdirectories'):
         t.decompose()
 
     # Use golang.org for external resources (such as CSS and JS)
+    # Use github.com for source files
     for t in soup.find_all(href=re.compile(r'^/')):
-        t['href'] = '//golang.org' + t['href']
+        href = t['href']
+        if href.endswith(".css"):
+            t['href'] = convert_path(href, base_css, "/")
+        elif href.startswith("/src/"):
+            t['href'] = convert_path(href, base_src, "/confluent-kafka-go/")
+        elif href.startswith("/pkg/"):
+            t['href'] = convert_path(href, base_pkg, "/pkg/")
 
     for t in soup.find_all(src=re.compile(r'^/')):
-        t['src'] = '//golang.org' + t['src']
+        if t['src'].endswith(".js"):
+            t['src'] = convert_path(t['src'], base_js, "/")
 
     # Write updated HTML to stdout
     print(soup.prettify())
-
