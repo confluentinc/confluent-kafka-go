63c9b7045b12218428b77e5327edb828d324cf47
Jos Visser <josvisser66@gmail.com>
2022-09-06 20:55:37 +0200

    Support for Linux glibc amd64

--- kafka/build_glibc_linux.go
+++ kafka/build_glibc_linux.go
@@ -6,7 +6,7 @@
 package kafka
 
 // #cgo CFLAGS: -DUSE_VENDORED_LIBRDKAFKA -DLIBRDKAFKA_STATICLIB
-// #cgo LDFLAGS: ${SRCDIR}/librdkafka_vendor/librdkafka_glibc_linux.a  -lm -ldl -lpthread -lrt
+// #cgo LDFLAGS: ${SRCDIR}/librdkafka_vendor/librdkafka_glibc_linux.a  -lm -ldl -lpthread -lrt -lssl -lcrypto -lz
 import "C"
 
 // LibrdkafkaLinkInfo explains how librdkafka was linked to the Go client
