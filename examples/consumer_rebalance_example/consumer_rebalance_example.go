b033dccbb5dea72f6894f1e7fff5a1a21eaccffc
Milind L <miluthra@confluent.io>
2023-02-10 12:31:33 +0530

    Change library version

--- examples/consumer_rebalance_example/consumer_rebalance_example.go
+++ examples/consumer_rebalance_example/consumer_rebalance_example.go
@@ -1,5 +1,5 @@
 /**
- * Copyright 2022 Confluent Inc.
+ * Copyright 2023 Confluent Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,7 @@ import (
 	"os"
 	"os/signal"
 
-	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
 func main() {
