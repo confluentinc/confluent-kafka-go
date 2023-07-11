fcfa5f9f408c5aa53bd39494b0a0820f4bfcc6a0
mahajanadhitya <115617755+mahajanadhitya@users.noreply.github.com>
2023-07-11 10:20:38 +0530

    [KIP-554] Add User SCRAM credentials API (#1004)
    ---------
    
    Co-authored-by: Emanuele Sabellico <esabellico@confluent.io>
    Co-authored-by: Milind L <miluthra@confluent.io>
    Co-authored-by: Milind L <milindl@users.noreply.github.com>

--- kafka/adminoptions.go
+++ kafka/adminoptions.go
@@ -133,7 +133,10 @@ func (ao AdminOptionRequestTimeout) supportsListConsumerGroupOffsets() {
 }
 func (ao AdminOptionRequestTimeout) supportsAlterConsumerGroupOffsets() {
 }
-
+func (ao AdminOptionRequestTimeout) supportsDescribeUserScramCredentials() {
+}
+func (ao AdminOptionRequestTimeout) supportsAlterUserScramCredentials() {
+}
 func (ao AdminOptionRequestTimeout) apply(cOptions *C.rd_kafka_AdminOptions_t) error {
 	if !ao.isSet {
 		return nil
@@ -412,6 +415,22 @@ type ListConsumerGroupOffsetsAdminOption interface {
 // See SetAdminRequestTimeout.
 type AlterConsumerGroupOffsetsAdminOption interface {
 	supportsAlterConsumerGroupOffsets()
+	apply(cOptions *C.rd_kafka_AdminOptions_t) error
+}
+
+// DescribeUserScramCredentialsAdminOption - see setter.
+//
+// See SetAdminRequestTimeout.
+type DescribeUserScramCredentialsAdminOption interface {
+	supportsDescribeUserScramCredentials()
+	apply(cOptions *C.rd_kafka_AdminOptions_t) error
+}
+
+// AlterUserScramCredentialsAdminOption - see setter.
+//
+// See SetAdminRequestTimeout.
+type AlterUserScramCredentialsAdminOption interface {
+	supportsAlterUserScramCredentials()
 	apply(cOptions *C.rd_kafka_AdminOptions_t) error
 }
 
