57040a4767a9b033b8336ee03a91b228d579738b
mahajanadhitya <amahajan@confluent.io>
2023-10-11 14:44:08 +0530

    Updated Example

--- examples/admin_list_offsets/admin_list_offsets.go
+++ examples/admin_list_offsets/admin_list_offsets.go
@@ -21,14 +21,52 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"time"
+	"strconv"
 
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
 func main() {
 
-	bootstrapServers := "localhost:9092"
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr,
+			"Usage: %s <bootstrap-servers> <topicname> <partition> <EARLIEST/LATEST/MAXTIMESTAMP/TIMESTAMP t1> ..\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	bootstrapServers := os.Args[1]
+
+	args := len(os.Args)
+	i := 2
+	topicPartitionOffsets := make(map[kafka.TopicPartition]kafka.OffsetSpec)
+	for i < args {
+		topicName := os.Args[i]
+		partition, err := strconv.Atoi(os.Args[i+1])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Invalid partition: %s\n", err)
+			os.Exit(1)
+		}
+		tp := kafka.TopicPartition{Topic: &topicName, Partition: int32(partition)}
+		if os.Args[i+2] == "EARLIEST" {
+			topicPartitionOffsets[tp] = kafka.EarliestOffsetSpec
+		} else if os.Args[i+2] == "LATEST" {
+			topicPartitionOffsets[tp] = kafka.LatestOffsetSpec
+		} else if os.Args[i+2] == "MAXTIMESTAMP" {
+			topicPartitionOffsets[tp] = kafka.MaxTimestampOffsetSpec
+		} else if os.Args[i+2] == "TIMESTAMP" {
+			timestamp, timestampErr := strconv.Atoi(os.Args[i+3])
+			if timestampErr != nil {
+				fmt.Fprintf(os.Stderr, "Invalid timestamp: %s\n", timestampErr)
+				os.Exit(1)
+			}
+			topicPartitionOffsets[tp] = kafka.NewOffsetSpecOfTimestamp(int64(timestamp))
+			i = i + 1
+		} else {
+			fmt.Fprintf(os.Stderr, "Invalid OffsetSpec.\n")
+			os.Exit(1)
+		}
+		i = i + 3
+	}
 
 	// Create a new AdminClient.
 	// AdminClient can also be instantiated using an existing
@@ -39,16 +77,12 @@ func main() {
 		fmt.Printf("Failed to create Admin client: %s\n", err)
 		os.Exit(1)
 	}
+	defer a.Close()
 
 	// Contexts are used to abort or limit the amount of time
 	// the Admin call blocks waiting for a result.
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-
-	topicPartitionOffsets := make(map[kafka.TopicPartition]kafka.OffsetSpec)
-	goTopic := "topicname"
-	tp1 := kafka.TopicPartition{Topic: &goTopic, Partition: 0}
-	topicPartitionOffsets[tp1] = kafka.EarliestOffsetSpec
 
 	results, err := a.ListOffsets(ctx, topicPartitionOffsets, kafka.SetAdminIsolationLevel(kafka.ReadCommitted))
 	if err != nil {
@@ -65,103 +99,5 @@ func main() {
 			fmt.Printf("	Offset : %d Timestamp : %d\n\n", info.Offset, info.Timestamp)
 		}
 	}
-	var topics []kafka.TopicSpecification
-	topics = append(topics, kafka.TopicSpecification{Topic: goTopic, NumPartitions: 1, ReplicationFactor: 1})
-	create_topic_result, create_topic_err := a.CreateTopics(ctx, topics)
-	if create_topic_err != nil {
-		fmt.Printf("Failed to create topic : %v\n", err)
-		os.Exit(1)
-	} else {
-		if create_topic_result[0].Error.Code() != 0 {
-			fmt.Printf("Failed to create Topic : %s with error : %s \n", create_topic_result[0].Topic, create_topic_result[0].Error.String())
-		} else {
-			fmt.Printf("Topic : %s created!\n", create_topic_result[0].Topic)
-		}
-	}
 
-	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": bootstrapServers})
-
-	if err != nil {
-		fmt.Printf("Failed to create producer: %s\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("Created Producer %v\n", p)
-	timestamp := time.Now()
-	t1 := timestamp.Add(time.Second * 100)
-	t2 := timestamp.Add(time.Second * 300)
-	t3 := timestamp.Add(time.Second * 200)
-	p.Produce(&kafka.Message{
-		TopicPartition: kafka.TopicPartition{Topic: &goTopic, Partition: 0},
-		Value:          []byte("Message-1"),
-		Headers:        []kafka.Header{{Key: "myTestHeader", Value: []byte("header values are binary")}},
-		Timestamp:      t1,
-	}, nil)
-
-	p.Produce(&kafka.Message{
-		TopicPartition: kafka.TopicPartition{Topic: &goTopic, Partition: 0},
-		Value:          []byte("Message-2"),
-		Headers:        []kafka.Header{{Key: "myTestHeader", Value: []byte("header values are binary")}},
-		Timestamp:      t2,
-	}, nil)
-
-	p.Produce(&kafka.Message{
-		TopicPartition: kafka.TopicPartition{Topic: &goTopic, Partition: 0},
-		Value:          []byte("Message-3"),
-		Headers:        []kafka.Header{{Key: "myTestHeader", Value: []byte("header values are binary")}},
-		Timestamp:      t3,
-	}, nil)
-
-	p.Flush(5 * 1000)
-	topicPartitionOffsets[tp1] = kafka.EarliestOffsetSpec
-	results, err = a.ListOffsets(ctx, topicPartitionOffsets, kafka.SetAdminIsolationLevel(kafka.ReadCommitted))
-	if err != nil {
-		fmt.Printf("Failed to List offsets: %v\n", err)
-		os.Exit(1)
-	}
-	for tp, info := range results.Results {
-		fmt.Printf("Topic: %s Partition_Index : %d\n", *tp.Topic, tp.Partition)
-		if info.Error.Code() != 0 {
-			fmt.Printf("	ErrorCode : %d ErrorMessage : %s\n\n", info.Error.Code(), info.Error.String())
-		} else {
-			fmt.Printf("	Offset : %d Timestamp : %d\n\n", info.Offset, info.Timestamp)
-		}
-	}
-	topicPartitionOffsets[tp1] = kafka.LatestOffsetSpec
-	results, err = a.ListOffsets(ctx, topicPartitionOffsets, kafka.SetAdminIsolationLevel(kafka.ReadCommitted))
-	if err != nil {
-		fmt.Printf("Failed to List offsets: %v\n", err)
-		os.Exit(1)
-	}
-	for tp, info := range results.Results {
-		fmt.Printf("Topic: %s Partition_Index : %d\n", *tp.Topic, tp.Partition)
-		if info.Error.Code() != 0 {
-			fmt.Printf("	ErrorCode : %d ErrorMessage : %s\n\n", info.Error.Code(), info.Error.String())
-		} else {
-			fmt.Printf("	Offset : %d Timestamp : %d\n\n", info.Offset, info.Timestamp)
-		}
-	}
-	topicPartitionOffsets[tp1] = kafka.MaxTimestampOffsetSpec
-	results, err = a.ListOffsets(ctx, topicPartitionOffsets, kafka.SetAdminIsolationLevel(kafka.ReadCommitted))
-	if err != nil {
-		fmt.Printf("Failed to List offsets: %v\n", err)
-		os.Exit(1)
-	}
-	for tp, info := range results.Results {
-		fmt.Printf("Topic: %s Partition_Index : %d\n", *tp.Topic, tp.Partition)
-		if info.Error.Code() != 0 {
-			fmt.Printf("	ErrorCode : %d ErrorMessage : %s\n\n", info.Error.Code(), info.Error.String())
-		} else {
-			fmt.Printf("	Offset : %d Timestamp : %d\n\n", info.Offset, info.Timestamp)
-		}
-	}
-	var del_topics []string
-	del_topics = append(del_topics, goTopic)
-	_, err = a.DeleteTopics(ctx, del_topics)
-	if err != nil {
-		fmt.Printf("Failed to delete topic: %s\n", err)
-		os.Exit(1)
-	}
-	a.Close()
-	p.Close()
 }
