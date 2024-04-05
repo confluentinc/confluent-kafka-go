4a3f372858ee7e1e058f7cdf4a7f1fb820ecdd23
Nathan Wienand <nathan.wienand@gmail.com>
2024-04-05 17:14:33 +0100

    fix unhandled error in consumer_example.go thank @TheAnthonyGG for this.

--- examples/consumer_example/consumer_example.go
+++ examples/consumer_example/consumer_example.go
@@ -69,6 +69,9 @@ func main() {
 	fmt.Printf("Created Consumer %v\n", c)
 
 	err = c.SubscribeTopics(topics, nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to subscribe to topics: %v\n", topics)
+	}
 
 	run := true
 
