8c445a50638c99bcb287b700baf19e1ffa11a790
PratRanj07 <pranjan@confluent.io>
2024-05-02 13:49:01 +0530

    Minor Change

--- kafka/adminoptions.go
+++ kafka/adminoptions.go
@@ -51,6 +51,8 @@ func (ao AdminOptionOperationTimeout) supportsDeleteTopics() {
 }
 func (ao AdminOptionOperationTimeout) supportsCreatePartitions() {
 }
+func (ao AdminOptionOperationTimeout) supportsDeleteRecords(){
+}
 
 func (ao AdminOptionOperationTimeout) apply(cOptions *C.rd_kafka_AdminOptions_t) error {
 	if !ao.isSet {
