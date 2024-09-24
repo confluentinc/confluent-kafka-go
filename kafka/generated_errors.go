0e86e8b7285c2b7915ddf5a5a0cf7a48b8623bbe
Milind L <milindl@users.noreply.github.com>
2024-09-24 20:28:07 +0530

    Update docs and errors for v2.5.4 (#1301)

--- kafka/generated_errors.go
+++ kafka/generated_errors.go
@@ -1,7 +1,7 @@
 package kafka
 
 // Copyright 2016-2024 Confluent Inc.
-// AUTOMATICALLY GENERATED ON 2024-09-02 16:16:32.144950005 +0530 IST m=+0.000136202 USING librdkafka 2.5.3
+// AUTOMATICALLY GENERATED ON 2024-09-24 09:39:38.47614636 +0530 IST m=+0.000123498 USING librdkafka 2.5.3
 
 /*
 #include "select_rdkafka.h"
