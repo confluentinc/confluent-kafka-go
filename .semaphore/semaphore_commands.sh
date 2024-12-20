bf51d3222620a36bbd034cc291a2a1808789886d
Kunal Gupta <kugupta@confluent.io>
2024-12-20 15:31:06 -0500

    DGS-16859: Run SonarQube report in pipeline (#1362)

--- .semaphore/semaphore_commands.sh
+++ .semaphore/semaphore_commands.sh
@@ -1,8 +1,12 @@
 set -e
-if [ "$EXPECT_LINK_INFO" = "dynamic" ]; then export GO_TAGS="-tags dynamic"; bash mk/bootstrap-librdkafka.sh ${LIBRDKAFKA_VERSION} tmp-build; fi
+coverage_profile="static_coverage.txt"
+if [ "$EXPECT_LINK_INFO" = "dynamic" ]; then export GO_TAGS="-tags dynamic" && coverage_profile="dynamic_coverage.txt"; bash mk/bootstrap-librdkafka.sh ${LIBRDKAFKA_VERSION} tmp-build; fi
 for dir in kafka examples ; do (cd $dir && go install $GO_TAGS ./...) ; done
 if [[ -f .do_lint ]]; then golint -set_exit_status ./examples/... ./kafka/... ./kafkatest/... ./soaktest/... ./schemaregistry/...; fi
-for dir in kafka schemaregistry ; do (cd $dir && go test -timeout 180s -v $GO_TAGS ./...) ; done
+for dir in kafka schemaregistry ; do (cd $dir && go test -coverprofile="$coverage_profile" -timeout 180s -v $GO_TAGS ./...) ; done
 go-kafkacat --help
 library-version
 (library-version | grep "$EXPECT_LINK_INFO") || (echo "Incorrect linkage, expected $EXPECT_LINK_INFO" ; false)
+
+(gocovmerge $(find . -type f -iname "*coverage.txt") > ${coverage_profile}) || (echo "Failed to merge coverage files" && exit 0)
+artifact push workflow ${coverage_profile} || true
