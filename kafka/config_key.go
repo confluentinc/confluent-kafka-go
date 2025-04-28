43972a96974551a5b083ac2d0f72121588227c0e
Enzo Lanzellotti <enzo.maxpayne@gmail.com>
2025-04-28 14:50:14 -0300

    fix: AutoOffsetRest name

--- kafka/config_key.go
+++ kafka/config_key.go
@@ -1,7 +1,7 @@
 package kafka
 
 const (
-	AutoOffSetReset                              ConfigKey = "auto.offset.reset"
+	AutoOffsetReset                              ConfigKey = "auto.offset.reset"
 	Acks                                         ConfigKey = "acks"
 	AutoIncludeJmxReporter                       ConfigKey = "auto.include.jmx.reporter"
 	BatchSize                                    ConfigKey = "batch.size"
