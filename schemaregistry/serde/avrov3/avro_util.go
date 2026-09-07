646adee151d2c4abe59d84db35de7307c23aeba7
Robert Yokota <rayokota@gmail.com>
2026-09-07 00:07:29 -0700

    Fix avrov3 RefSchema gap

--- schemaregistry/serde/avrov3/avro_util.go
+++ schemaregistry/serde/avrov3/avro_util.go
@@ -42,6 +42,10 @@ func transform(ctx serde.RuleContext, resolver *avro.TypeResolver, schema avro.S
 		fieldCtx.Type = getType(schema)
 	}
 	switch schema.(type) {
+	case *avro.RefSchema:
+		// A reference to a named type has to be unwrapped, or the inline tags on the record
+		// it points at - and so the fields they mark for encryption - are never seen.
+		return transform(ctx, resolver, schema.(*avro.RefSchema).Schema(), msg, fieldTransform)
 	case *avro.UnionSchema:
 		subschema, submsg, err := resolveUnion(resolver, schema, msg)
 		if err != nil {
