b0372b9079bf0e5415cfc7c55cd42b6d99946a60
Robert Yokota <rayokota@gmail.com>
2026-09-08 16:09:52 -0700

    Fix variant doc

--- schemaregistry/rules/cel/decimal_util.go
+++ schemaregistry/rules/cel/decimal_util.go
@@ -103,8 +103,18 @@ func signedBytesFromBigInt(i *big.Int) []byte {
 		}
 		return b
 	default:
-		length := uint(i.BitLen()/8+1) * 8
-		return new(big.Int).Add(i, new(big.Int).Lsh(big.NewInt(1), length)).Bytes()
+		// A negative value's magnitude is Not(i) == -i-1, so its bit length is one less at every
+		// exact signed boundary. Sizing from i.BitLen() emitted ff80 for -128 where
+		// BigInteger.toByteArray gives 80.
+		bits := new(big.Int).Not(i).BitLen() + 1
+		byteLen := (bits + 7) / 8
+		if byteLen < 1 {
+			byteLen = 1
+		}
+		shifted := new(big.Int).Add(i, new(big.Int).Lsh(big.NewInt(1), uint(byteLen)*8))
+		out := make([]byte, byteLen)
+		shifted.FillBytes(out)
+		return out
 	}
 }
 
